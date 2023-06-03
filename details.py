47afbdc7a277a9b5b85f4064684f00f43e436d22
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2023-06-03 10:45:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 18516364
-api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
+api_id = 27584273
+api_hash = "1fcbb51a52ae000357b189482cdd7179"
+bot_token = "5877303679:AAFePQ6v1IznygtUuMFPsWWTXWff865y02Q"
 
